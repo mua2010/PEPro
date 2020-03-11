9452a8e4f514b77abdf99913d3bbea8be40a9693
unknown <mumairafzal2010@gmail.com>
2020-03-11 15:18:00 -0400

    commented out django_heroku

--- pepro/settings.py
+++ pepro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,4 +130,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
