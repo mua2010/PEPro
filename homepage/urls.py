7b24b16231d5a36340ffa199e53fb1bcfb3b1790
Josh Viktorov <joshviktorov27@gmail.com>
2020-03-27 15:46:00 -0400

    hello world

--- homepage/urls.py
+++ homepage/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from homepage import views
 
 urlpatterns = [
-    path('', include('homepage.urls')),
-    path('admin/', admin.site.urls),
+    path('home', views.display, name="display"),
 ]
