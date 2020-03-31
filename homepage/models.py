d0add269c795f45727d60c42b510bc4f0945bdae
test-2010 <mumairafzal2010@gmail.com>
2020-03-31 01:05:38 -0400

    added models and db migrations

--- homepage/models.py
+++ homepage/models.py
@@ -8,29 +8,34 @@ class Company(models.Model):
         return self.company_name
 
 class Employee(models.Model):
-    email = models.CharField(default=None, primary_key=True, max_length=70)
+    auto_increment_id = models.AutoField(primary_key=True)
     employee_id = models.IntegerField()
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=35)
     manager_id = models.IntegerField(blank=True, null=True)
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_id2company_id')
+    email = models.CharField(max_length=70)
 
     def __str__(self):
         full_name = "%s %s" %(self.first_name, self.last_name)
         return full_name
 
 class Review(models.Model):
-    # The employee writing the review
-    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2email', default=None)
-    # The employee that requested to have a review written about them
-    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2email', default=None)
+    # reviewer: The employee writing the review
+    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2employee_id', default=None)
+    # reviewee: The employee that requested to have a review written about them
+    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2employee_id', default=None)
     review_text = models.CharField(max_length=10000, default=None)
-    review_created_at = models.DateTimeField(default=None, blank=True)
+    review_requested_at = models.DateTimeField(auto_now_add=True)
+    review_edited_at = models.DateTimeField(auto_now=True, blank=True)
     
+    # P => accpeted or declined?
+    PENDING = 'P'
     EDITING = 'E'
     SENT = 'S'
     
     STATUS_CHOICES = (
+        (PENDING, 'pending'),
         (EDITING, 'editing'),
         (SENT, 'sent')
     )
@@ -42,10 +47,3 @@ class Review(models.Model):
     
     def __str__(self):
         return self.review_text
-
-class Request(models.Model):
-    request_created_at = models.DateTimeField(auto_now_add=True)
-    # The employee writing the review
-    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='request_reviewer2email')
-    # The employee that requested to have a review written about them
-    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='request_reviewee2email')
