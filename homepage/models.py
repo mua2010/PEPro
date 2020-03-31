9fffa43bb482a58a2328bacfeed9d25005803f67
test-2010 <mumairafzal2010@gmail.com>
2020-03-30 22:43:43 -0400

    new models and db migrations

--- homepage/models.py
+++ homepage/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
-import datetime
-
 class Company(models.Model):
     company_id = models.IntegerField(default=None, primary_key=True)
-    company_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=70)
 
     def __str__(self):
         return self.company_name
 
 class Employee(models.Model):
-    employee_id = models.IntegerField(default=None, primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    email = models.CharField(default=None, primary_key=True, max_length=70)
+    employee_id = models.IntegerField()
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
     manager_id = models.IntegerField(blank=True, null=True)
     company_id = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_id2company_id')
 
@@ -21,17 +20,19 @@ class Employee(models.Model):
         return full_name
 
 class Review(models.Model):
-    authored_by = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='authored_by2employee_id')
-    recipient = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='recipient2employee_id')
-    review_text = models.CharField(max_length=10000)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
-    requested_at = models.DateTimeField(auto_now_add=True)
+    # The employee writing the review
+    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2email', default=None)
+    # The employee that requested to have a review written about them
+    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2email', default=None)
+    review_text = models.CharField(max_length=10000, default=None)
+    review_created_at = models.DateTimeField(default=None, blank=True)
     
     EDITING = 'E'
     SENT = 'S'
+    
     STATUS_CHOICES = (
         (EDITING, 'editing'),
-        (SENT, 'sent'),
+        (SENT, 'sent')
     )
     status = models.CharField(
         max_length=10,
@@ -41,3 +42,10 @@ class Review(models.Model):
     
     def __str__(self):
         return self.review_text
+
+class Request(models.Model):
+    request_created_at = models.DateTimeField(auto_now_add=True)
+    # The employee writing the review
+    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='request_reviewer2email')
+    # The employee that requested to have a review written about them
+    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='request_reviewee2email')
