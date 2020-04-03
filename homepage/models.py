8cbfcfb94d3efdb26906dd0f109f59a14c9b901d
test-2010 <mumairafzal2010@gmail.com>
2020-04-03 00:14:15 -0400

    updated models.py

--- homepage/models.py
+++ homepage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 import datetime
 
 class Company(models.Model):
-    company_id = models.IntegerField(default=None, primary_key=True)
+    company_id = models.IntegerField(primary_key=True)
     company_name = models.CharField(max_length=70)
 
     def __str__(self):
@@ -11,11 +11,11 @@ class Company(models.Model):
 
 class Employee(models.Model):
     email = models.CharField(max_length=70)
-    employee_id = models.IntegerField(default=None)
+    employee_id = models.IntegerField()
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=35)
     manager_id = models.IntegerField(blank=True, null=True)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_id2company_id')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company2company_id')
 
     def __str__(self):
         full_name = "%s %s" %(self.first_name, self.last_name)
@@ -23,9 +23,9 @@ class Employee(models.Model):
 
 class Review(models.Model):
     # The employee writing the review
-    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2email')
+    reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2id')
     # The employee that requested to have a review written about them
-    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2email')
+    reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2id')
     review_text = models.CharField(max_length=10000, default=None)
     review_created_at = models.DateTimeField(default=None, blank=True)
     
@@ -48,9 +48,9 @@ class Review(models.Model):
 class Request(models.Model):
     request_created_at = models.DateTimeField(auto_now_add=True, blank=True)
     # The employee writing the review
-    request_reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewer2employee_id')
+    request_reviewer = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='req_reviewer2id')
     # The employee that requested to have a review written about them
-    request_reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviewee2employee_id')
+    request_reviewee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='req_reviewee2id')
 
     def __str__(self):
         return str(self.request_reviewee) + "'s review request to " + str(self.request_reviewer)
