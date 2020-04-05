e4c64ca7d0cd2d5ea6c31d7586d32d59a9b2c345
test-2010 <mumairafzal2010@gmail.com>
2020-04-04 20:04:31 -0400

    added new models, db migrations. request,display features

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Employee, Company, Review
+from .models import Employee, Company, Review, Request
 
 admin.site.register(Employee)
 admin.site.register(Company)
 admin.site.register(Review)
+admin.site.register(Request)
