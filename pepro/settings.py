c6f47902abc6fbfe946d5a6d341a9b16e00d7f46
Umair <mumairafzal2010@gmail.com>
2020-04-15 13:19:16 -0400

    added whitenoise middleware for serving static files in production

--- pepro/settings.py
+++ pepro/settings.py
@@ -63,6 +63,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pepro.urls'
