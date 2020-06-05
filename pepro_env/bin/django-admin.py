a5ad0f131d0a09f99a8364e670520c16130fb500
root <root@DESKTOP-1G4E1Q4.localdomain>
2020-06-05 19:55:22 -0400

    new changes

---
+++ pepro_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/Umair Afzal/OneDrive/Data Dec 2019/laptop data/Study stuff/6th Semester Spring2020/CS320/Project/PEPro/pepro_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
